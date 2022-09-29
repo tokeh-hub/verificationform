f10183413c1d0a0ab74a1feacf7e83c4043f3198
tokeh-hub <olatunjiaisha12@gmail.com>
2022-09-29 14:06:07 +0100

    added few console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 // import useAxios from './useAxios';
-
+import { useParams } from 'react-router-dom';
+import {useEffect, useState} from 'react'
+import useGeolocation from './useGeolocation';
 function App() {
-  // const api = useAxios()
+  const {id} = useParams()
+  const [password,setPassword] = useState('')
+  const api = useAxios()
+  const location = useGeolocation()
+  
+   const verifyForm = async (e) =>{
+    e.preventDefault()
+    try {
+      const response = await api.post(`staff/confirm`,{
+        employeeId: id,
+        password : password,
+        location: location.coordinates
+      })
+      console.log(response)
+    } catch (error) {
+      
+    }
+   }
 
+   useEffect(()=>{
+    console.log('id',id)
+    console.log('location',location)
+   },[])
 
   return (
-    <form className="">
-           <input type='password' name='password' placeholder='Enter Password'/>
+    <form onSubmit={verifyForm} className="">
+           <input type='password' name='password' placeholder='Enter Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
     </form>
   );
 }
